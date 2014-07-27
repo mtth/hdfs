b9e6e7facc9207699929edbc8904fb5afd9e8be1
Matthieu Monsch <monsch@alum.mit.edu>
2014-07-27 11:43:43 -0700

    Updated package files.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-"""HdfsCLI."""
+"""HdfsCLI: a command line interface for WebHDFS."""
 
 from hdfs import __version__
 from setuptools import find_packages, setup
 
+
 setup(
     name='hdfs',
     version=__version__,
-    description='HdfsCLI',
+    description=__doc__,
     long_description=open('README.rst').read(),
     author='Matthieu Monsch',
     author_email='monsch@alum.mit.edu',
@@ -20,6 +21,8 @@ setup(
       'Intended Audience :: Developers',
       'License :: OSI Approved :: MIT License',
       'Programming Language :: Python',
+      'Programming Language :: Python :: 2.6',
+      'Programming Language :: Python :: 2.7',
     ],
     install_requires=[
       'docopt',
