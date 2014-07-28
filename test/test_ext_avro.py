1ab24a99b803aeeb05daa686ad4953be26877275
Matthieu Monsch <monsch@alum.mit.edu>
2014-07-27 23:39:15 -0700

    Fixed and added Avro tests.

--- test/test_ext_avro.py
+++ test/test_ext_avro.py
@@ -3,6 +3,7 @@
 
 """Test HdfsAvro extension."""
 
+from avro.io import AvroTypeException
 from hdfs.ext.avro import *
 from helpers import _TestSession
 from nose.tools import *
@@ -12,12 +13,20 @@ class TestWriter(_TestSession):
 
   def setup(self):
     super(TestWriter, self).setup()
-    self.writer = AvroWriter(self.client, 'aw.avro')
+    if self.client:
+      self.writer = AvroWriter(self.client, 'aw.avro')
 
-  def test_simple(self):
-    self.writer.records.send({'key': 'value1'})
-    self.writer.records.send({'key': 'value2', 'bar': 'that'})
+  def test_write_inferring_schema(self):
+    self.writer.records.send({'foo': 'value1'})
+    self.writer.records.send({'foo': 'value2', 'bar': 'that'})
     self.writer.records.close()
     data = self.client._open('aw.avro').content
+    ok_('foo' in data)
     ok_('value1' in data)
     ok_('value2' in data)
+    ok_(not 'that' in data)
+
+  @raises(AvroTypeException)
+  def test_invalid_schema(self):
+    self.writer.records.send({'foo': 'value1'})
+    self.writer.records.send({'bar': 'value2'})
