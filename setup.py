d399ccfef587280e4964550afe680268292d9ed5
Matthieu Monsch <monsch@alum.mit.edu>
2015-04-06 17:58:38 -0700

    Change pandas required version to lower bound.

--- setup.py
+++ setup.py
@@ -11,34 +11,34 @@ from setuptools import find_packages, setup
 ENTRY_POINT = environ.get('HDFS_ENTRY_POINT', 'hdfs')
 
 setup(
-    name='hdfs',
-    version=__version__,
-    description=__doc__,
-    long_description=open('README.rst').read(),
-    author='Matthieu Monsch',
-    author_email='monsch@alum.mit.edu',
-    url='http://hdfscli.readthedocs.org',
-    license='MIT',
-    packages=find_packages(),
-    classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: MIT License',
-      'Programming Language :: Python',
-      'Programming Language :: Python :: 2.6',
-      'Programming Language :: Python :: 2.7',
-    ],
-    install_requires=[
-      'docopt',
-      'requests>=2.0.1',
-    ],
-    extras_require={
-      'avro': ['avro'],
-      'kerberos': ['requests-kerberos'],
-      'dataframe': ['numpy', 'pandas==0.14.1', 'fastavro'],
-    },
-    entry_points={'console_scripts': [
-      '%s = hdfs.__main__:main' % (ENTRY_POINT, ),
-      '%savro = hdfs.ext.avro:main' % (ENTRY_POINT, ),
-    ]},
+  name='hdfs',
+  version=__version__,
+  description=__doc__,
+  long_description=open('README.rst').read(),
+  author='Matthieu Monsch',
+  author_email='monsch@alum.mit.edu',
+  url='http://hdfscli.readthedocs.org',
+  license='MIT',
+  packages=find_packages(),
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+  ],
+  install_requires=[
+    'docopt',
+    'requests>=2.0.1',
+  ],
+  extras_require={
+    'avro': ['avro'],
+    'kerberos': ['requests-kerberos'],
+    'dataframe': ['numpy', 'pandas>=0.14.1', 'fastavro'],
+  },
+  entry_points={'console_scripts': [
+    '%s = hdfs.__main__:main' % (ENTRY_POINT, ),
+    '%savro = hdfs.ext.avro:main' % (ENTRY_POINT, ),
+  ]},
 )
