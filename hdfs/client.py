9509cfbeed0ede860913d89ce90cddc5d2885353
Leonel Atencio <leo.atencio@gmail.com>
2016-10-03 15:49:00 -0500

    test

--- hdfs/client.py
+++ hdfs/client.py
@@ -307,6 +307,7 @@ class Client(object):
       'Setting ACLSPEC %r for %r.', aclspec, hdfs_path
     )
     res = self._set_acl(hdfs_path, aclspec=aclspec)
+    print('res is: ', res)
     if res is not None:
       raise HdfsError('Check your path or aclspec string', hdfs_path)
 
