0ad8cd3337ae79e2035fd37b7c9f8948f989fb8a
Matthieu Monsch <matthieu.monsch@gmail.com>
2018-11-20 19:08:43 +0100

    Set exception message explicitly
    It looks like this is recommended now
    https://stackoverflow.com/a/28226376/1062617. This should also fix
    https://github.com/mtth/hdfs/issues/81.

--- hdfs/util.py
+++ hdfs/util.py
@@ -26,7 +26,8 @@ class HdfsError(Exception):
   """
 
   def __init__(self, message, *args, **kwargs):
-    super(HdfsError, self).__init__(message % args if args else message)
+    self.message = message % args if args else message
+    super(HdfsError, self).__init__(self.message)
     self.exception = kwargs.get("exception")
 
 
