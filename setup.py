cc558306e949527b9bb1183ba64f3d76962483a7
Matthieu Monsch <matthieu.monsch@gmail.com>
2023-08-02 09:58:25 -0700

    Update

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
